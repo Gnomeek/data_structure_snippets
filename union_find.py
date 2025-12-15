b026d457a16ad40e1050ca031e02cce784a76bde
willzhao <gnomeek@outlook.com>
2025-12-15 13:33:51 +0800

    add UnionFind

---
+++ union_find.py
@@ -0,0 +1,29 @@
+class UF:
+    def __init__(self, size):
+        self.parent = list(range(size))
+        self.size = size
+        self.rank = [1] * size
+    
+    def find(self, x):
+        while x != self.parent[x]:
+            self.parent[x] = self.parent[self.parent[x]]
+        return x
+    
+    def union(self, x, y):
+        a, b = self.find(x), self.find(y)
+        if a == b:
+            return False
+        if self.rank[a] >= self.rank[b]:
+            self.parent[b] = a
+            self.rank[a] += self.rank[b]
+        else:
+            self.parent[a] = b
+            self.rank[b] += self.rank[a]
+        return True
+    
+    def group_size(self):
+        res = {}
+        for i in range(self.size):
+            parent = self.find(i)
+            res[parent] = self.rank[parent]
+        return res
